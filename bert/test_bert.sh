e42c21d6358d070106ed233109e576d93b5099fa
liyijin <liyijin@xcoresigma.com>
2024-08-23 20:22:47 +0800

    add visible device for bert/yolo

--- bert/test_bert.sh
+++ bert/test_bert.sh
@@ -1,5 +1,5 @@
 for i in {0..7}
 do
-    python main_fp16.py -id $i 2>&1 | tee bert.device_$i.log &
+    MUSA_VISIBLE_DEVICES=$i nohup python main_fp16.py -id $i 2>&1 | tee bert.device_$i.log &
 done
 
--- yolo/test_yolov5m.sh
+++ yolo/test_yolov5m.sh
@@ -2,5 +2,5 @@ python gen-onnx.py
 python convert.py
 for i in {0..7}
 do
-    python main.py -id $i 2>&1 | tee yolo.device_$i.log &
+    MUSA_VISIBLE_DEVICES=$i nohup python main.py -id $i 2>&1 | tee yolo.device_$i.log &
 done
