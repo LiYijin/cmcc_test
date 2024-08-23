bbbac12d2f309499062eee0c81ee650d414bf842
liyijin <liyijin@xcoresigma.com>
2024-08-23 20:19:15 +0800

    fix visible device for resnet

--- resnet50/test_resnet50.sh
+++ resnet50/test_resnet50.sh
@@ -1,5 +1,5 @@
 python gen-onnx.py -P fp16
 for i in {0..7}
 do
-    python main.py -P fp16 -id $i 2>&1 | tee resnet.device_$i.log &
+    MUSA_VISIBLE_DEVICES=$i nohup python main.py -P fp16 -id $i 2>&1 | tee resnet.device_$i.log &
 done
