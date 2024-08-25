d4897623c6cf95a2e47a6ed061d5469d19890ed2
liyijin <liyijin@xcoresigma.com>
2024-08-25 11:28:02 +0800

    fix yolo path bug

--- yolo/main.py
+++ yolo/main.py
@@ -83,14 +83,14 @@ def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
 
 if not os.path.exists("/dataset/annotations_trainval2017.zip"):
     os.system(
-        "wget -o /dataset/annotations_trainval2017.zip http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+        "wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip & cp annotations_trainval2017.zip /modesl/."
     )
 else:
     print("Use local annotation file")
 
 if not os.path.exists("/dataset/val2017.zip"):
     os.system(
-        "wget -o /dataset/val2017.zip http://images.cocodataset.org/zips/val2017.zip"
+        "wget http://images.cocodataset.org/zips/val2017.zip & cp annotations_trainval2017.zip /modesl/."
     )
 else:
     print("Use local val file")
