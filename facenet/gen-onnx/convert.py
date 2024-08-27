31841e00225c9c00934ef91b359637db451383c5
liyijin <liyijin@xcoresigma.com>
2024-08-27 14:43:27 +0800

    [docs] fix facenet README and add copy in gen-onnx script

--- facenet/gen-onnx/convert.py
+++ facenet/gen-onnx/convert.py
@@ -1,4 +1,8 @@
 from models.utils import tensorflow2pytorch
 import os
+if not os.path.exists("./data/20180408-102900/"):
+    os.system(
+        "cp -r /models/20180408-102900/  ./data/."
+    )
 
 tensorflow2pytorch.tensorflow2pytorch()
--- facenet/gen-onnx/gen-onnx.py
+++ facenet/gen-onnx/gen-onnx.py
@@ -1,12 +1,15 @@
 from models.inception_resnet_v1 import InceptionResnetV1
 import torch
+import os
 
 # Load pretrained resnet model
 resnet = InceptionResnetV1(
     classify=False,
     pretrained='casia-webface'
 )
-
+os.system("mkdir -p ../model && mkdir -p ../data")
+os.system("cp -r /dataset/lfw_mtcnnpy_160 ../data/")
+os.system("cp  /dataset/pairs.txt ../data/")
 random_input = torch.randn(64, 3, 160, 160)
 torch.onnx.export(resnet, random_input, "../model/facenet-fp32.onnx")
 
